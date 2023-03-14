8640a525c4d1fd717939e1e3d8475feecf2218de
Angelo2530 <fanirybelahy@gmail.com>
2023-03-14 22:49:54 +0300

    formulaire bouton

--- index.php
+++ index.php
@@ -5,9 +5,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Site Git</title>
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/all.min.css">
     <link rel="stylesheet" href="style.css">
+
 </head>
 <body>
     
+    <div class="container-fluid">
+        <div class="container bg1">
+            <form method="post">
+                <h1>Connexion</h1>
+                <label for="">Username</label>
+                <input type="text" name="user" id="user">
+                <label for="">Password</label>
+                <input type="password" name="psw" id="psw">
+                <input type="submit" value="se connecter">
+            </form>
+        </div>
+    </div>
+    <script src="./assets/js/jquery.min.js"></script>
+    <script src="./assets/js/boostrap.min.js"></script>
+    <script src="./assets/js/all.min.js"></script>
 </body>
 </html>
